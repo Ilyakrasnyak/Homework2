ba4a30254cdcd01788a5a10da3efe275efcd73c2
ilyakrasnyak <ilyakrasnyak@gmail.com>
2020-03-02 23:16:43 +0500

    Commit

--- external_sort/errors.py
+++ external_sort/errors.py
@@ -2,3 +2,8 @@ class LaunchParametersError(Exception):
     def __init__(self):
         self.txt = "You have entered unsupported quantities of parameters. Expected 3."
 
+
+class MemoryStarvationError(Exception):
+    def __init__(self):
+        self.txt = "Not enough memory to process the string"
+
