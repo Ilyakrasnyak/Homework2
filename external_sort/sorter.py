0bde2190c5556acb839cb5430d3d627b40a8f918
ilyakrasnyak <ilyakrasnyak@gmail.com>
2020-03-02 23:03:42 +0500

    Commit

--- external_sort/sorter.py
+++ external_sort/sorter.py
@@ -1,2 +1,45 @@
-class Sorter():
-    pass
+import os
+import logging
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(message)s'
+)
+
+
+class Sorter:
+
+    def __init__(self, max_size, input_file, output_file):
+        self.max_size = max_size
+        self.input_file = input_file
+        self.output_file = output_file
+        self.current_directory = os.getcwd()
+
+    @staticmethod
+    def get_file_location(self, file_name):
+        file_location = self.current_directory + '\\' + file_name
+        logging.debug('{} file is being processed'.format(file_location))
+        return file_location
+
+    def create_temporary_folder(self):
+        path = self.current_directory + '\\' + 'temporary_folder'
+        logging.debug('Create temporary folder {}'.format(path))
+        os.mkdir(path)
+
+    def shatter_file(self):
+        input_file_location = self.get_file_location(self, self.input_file)
+        with open(input_file_location) as input_file:
+            for line in input_file:
+                print(line)
+
+
+
+
+
+
+
+
+
+
+
